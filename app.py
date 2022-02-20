0bfb4ead1a0f50f3a7956ddbb99b43a954cdbccb
William L. Anderson <band@acm.org>
2022-02-20 10:33:22 -0600

    first jinja2 test

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
+
+
 @app.route('/')
 def home():
     return 'Flask with docker!'
@@ -7,3 +9,7 @@ def home():
 @app.route('/page-news')
 def page_news():
     return 'page-news has no new pages yet.'
+#
+@app.route('/ttest')
+def template_test():
+    return render_template('template.html', my_string="Tap, tap  ...  hello?")
