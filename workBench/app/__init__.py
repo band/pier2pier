f6edfe8eeed3878e28f3dc2d593897a92e80a2bb
William L. Anderson <band@acm.org>
2022-02-24 13:35:51 -0600

    need SECRET_KEY to flash messages

--- workBench/app/__init__.py
+++ workBench/app/__init__.py
@@ -3,6 +3,8 @@ from flask import Flask
 
 app = Flask(__name__)
 
+app.config["SECRET_KEY"] = "016hOdMleWZ4uJjt2CG0NmA"
+
 from app import views
 from app import admin_views
 from app import myviews
