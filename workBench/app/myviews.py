511a03aae70b4e9b36832c8d574e46d37178d78a
William L. Anderson <band@acm.org>
2022-02-24 13:36:37 -0600

    test flash message

--- workBench/app/myviews.py
+++ workBench/app/myviews.py
@@ -2,8 +2,10 @@
 from app import app
 
 from flask import render_template
+from flask import flash
 
-@app.route("/public/myviews")
+@app.route("/myviews")
 def imho():
+    flash("Sometimes I sit and think and sometimes I just sit ....")
     return render_template("public/myviews.html")
 
